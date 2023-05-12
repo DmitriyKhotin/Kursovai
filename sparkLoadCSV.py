65d9ccd42ac5bfdcc51cdae3f161faab207dbd8f
admin <admin@ubuntu04.myguest.virtualbox.org>
2023-05-12 23:00:18 +0300

    добавлен результат выполнения spark

--- sparkLoadCSV.py
+++ sparkLoadCSV.py
@@ -8,15 +8,15 @@ spark=SparkSession \
     .getOrCreate()
 spark.sparkContext.setLogLevel("WARN")
 passengerSchema = StructType([
-    StructField("id", IntegerType(), True),
+    StructField("id", StringType(), True),
     StructField("age", IntegerType(), True)
 ])
 ticketSchema = StructType([
-    StructField("race_id", IntegerType(), True),
-    StructField("passenger_id", IntegerType(), True)
+    StructField("race_id", StringType(), True),
+    StructField("passenger_id", StringType(), True)
 ])
 raceSchema = StructType([
-    StructField("_id", IntegerType(), True),
+    StructField("_id", StringType(), True),
     StructField("race_number", IntegerType(), True)
 ])
 df_passenger= spark.read.load("hdfs://localhost:9000/chapter5/passenger.csv", format="csv", sep=",", inferScema="true", header="true", schema=passengerSchema)
@@ -26,16 +26,16 @@ df_passenger.registerTempTable("passenger_t")
 df_race.registerTempTable("race_t")
 df_ticket.registerTempTable("ticket_t")
 
-df_sql = spark.sql(
+result_df = spark.sql(
     """
     SELECT race_t._id, COUNT(passenger_t.id)
     FROM race_t
     INNER JOIN ticket_t ON race_t._id = ticket_t.race_id
     INNER JOIN passenger_t ON passenger_t.id = ticket_t.passenger_id
-    WHERE passenger_t.age < 18
+    WHERE passenger_t.age < 12
     GROUP BY race_t._id
     """
 )
 
-df_sql.show()
+result_df.show(n=result_df.count(), truncate=False)
 input("Ctrl C")
